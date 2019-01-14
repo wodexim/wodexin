aa14b8ea766c337ebf1027c25069acba7df2c10f
liuneng <liuneng@163.com>
2019-01-14 20:13:34 +0800

    第一次提交

---
+++ first-git-test/src/com/itheima/test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("黑马itcast");
+        System.out.println("黑马itcast");
+        System.out.println("黑马itcast");
+        System.out.println("黑马itcast");
+        System.out.println("黑马itcast");
+        System.out.println("黑马itcast");
+    }
+}
